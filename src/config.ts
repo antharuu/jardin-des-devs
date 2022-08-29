2d7aa03395cdbdc30df4b16349b415db91258a7b
Anthony <antho.eclips@gmail.com>
2022-08-29 23:05:15 +0200

    Création du style de base

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jardin des devs';
+export const SITE_DESCRIPTION = 'Serveur communautaire de création du jeu vidéo, entraide, partage et fun sont primordiaux ici.  Si vous avez besoin d\'aide, nous sommes là et ferons notre nécessaire pour vous aider. Nous organisons régulièrement des débats, ateliers et games jams. Ici, c\'est tous les jours accueillant et de la bonne ambiance, alors n\'hésitez pas à nous rejoindre !';
---
+++ src/jdd/infos.js
@@ -0,0 +1,11 @@
+export const Categories = [
+    "unreal",
+    "unity",
+    "godot",
+    "jardin-des-devs"
+    // "gamemaker",
+    // "love2d",
+    // "monogame",
+    // "construct",
+    // "others",
+]
